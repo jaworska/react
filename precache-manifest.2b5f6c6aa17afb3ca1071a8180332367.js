93fe50a4a54e50ce7abf12c881e6c2b7356a0a8b
Asers <joanna.jaworska@invisio.digital>
2020-01-24 11:08:59 +0100

    Updates

---
+++ precache-manifest.2b5f6c6aa17afb3ca1071a8180332367.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6f31c7bbbf824b04f741d3c11f685b97",
+    "url": "/react/index.html"
+  },
+  {
+    "revision": "83f25655fb53c29e669c",
+    "url": "/react/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "2013f762cc75b795deb6",
+    "url": "/react/static/js/2.9c4ec06e.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/react/static/js/2.9c4ec06e.chunk.js.LICENSE"
+  },
+  {
+    "revision": "83f25655fb53c29e669c",
+    "url": "/react/static/js/main.8ec6fa4c.chunk.js"
+  },
+  {
+    "revision": "ae413fcad5732cdcccb4",
+    "url": "/react/static/js/runtime-main.7ed49187.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react/static/media/logo.5d5d9eef.svg"
+  }
+]);
